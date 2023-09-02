2954af6ee8c397e410f92360e5b70ae0406de2c7
Brian <bjjx1999@live.com>
2023-09-02 21:29:26 +0800

    Add demo

---
+++ Samples~/Scripts/HeroProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using SmartReference.Runtime;
+using UnityEngine;
+
+namespace SmartReference.Samples {
+    [Serializable]
+    public class AttackData {
+        public float baseDamage;
+        public float damageMultiplier;
+        public SmartReference<GameObject> effect;
+    }
+    
+    [CreateAssetMenu(fileName = "HeroProfile", menuName = "SmartReference/Samples/HeroProfile")]
+    public class HeroProfile: ScriptableObject {
+        public int id;
+        public string heroName;
+        public SmartReference<GameObject> prefab;
+        public SmartReference<Texture2D> icon;
+        public AttackData attackData;
+        public List<SmartReference<GameObject>> skillEffects;
+    }
+}
---
+++ Samples~/Scripts/Monster.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using SmartReference.Runtime;
+using UnityEngine;
+
+namespace SmartReference.Samples {
+    public class Monster: MonoBehaviour {
+        public int id;
+        public string monsterName;
+        public SmartReference<GameObject> prefab;
+        public SmartReference<Sprite> icon;
+        public SmartReference<GameObject>[] skillEffects;
+    }
+}
