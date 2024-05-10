f555775aedce0341c0019443737dfc16b461b814
Xiao Jiang <bjjx1999@live.com>
2024-05-10 21:54:28 +0800

    Fix resources problem

--- Runtime/Loaders/ResourcesLoader.cs
+++ Runtime/Loaders/ResourcesLoader.cs
@@ -5,12 +5,25 @@ using Object = UnityEngine.Object;
 namespace SmartReference.Runtime {
     public class ResourcesLoader: ISmartReferenceLoader {
         public Object Load(string path, Type type) {
-            return Resources.Load(path, type);
+            var resourcesPath = GetResourcesPath(path);
+            return Resources.Load(resourcesPath, type);
         }
 
         public void LoadAsync(string path, Type type, Action<Object> callback) {
-            var request = Resources.LoadAsync(path, type);
+            var resourcesPath = GetResourcesPath(path);
+            var request = Resources.LoadAsync(resourcesPath, type);
             request.completed += _ => callback?.Invoke(request.asset);
+        }
+        
+        private string GetResourcesPath(string path) {
+            var index = path.LastIndexOf("Resources/", StringComparison.Ordinal);
+            if (index == -1) {
+                Debug.LogError($"[SmartReference] ResourcesLoader: Path {path} is not in Resources folder");
+                return path;
+            }
+
+            var extensionIndex = path.LastIndexOf(".", StringComparison.Ordinal);
+            return path[(index + "Resources/".Length)..extensionIndex];
         }
     }
 }
